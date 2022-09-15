f0993221004c7afa33e6a3ca42a7d350ba389caf
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2022-09-15 04:05:05 +0100

    first javaScript app

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-let name = " Gabriel"
-let greeting = "Hi, my name is"
+// let welcomeEl = document.getElementById("welcome-el")
 
-let myGreeting = greeting + name 
-console.log(myGreeting)
+// let name = "Asha Gabriel"
+// let greeting = "Welcome back "
+
+// welcomeEl.innerText = greeting + name
+
+// welcomeEl.innerText += " .O"
 
 // initialize the count as 0
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count 
 
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
 
-let countEl = document.getElementById("count-el")
 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
 let saveBtn = document.getElementById("save-btn")
 
 function save() {
-    
+    let previousEntries = count + " - "
+    saveEl.textContent += previousEntries 
+    countEl.textContent = 0
+    count = 0
 }
